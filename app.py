dd38b3deb876db9a9cbe64fe95182bb621d0788e
jeon1109 <101310070+jeon1109@users.noreply.github.com>
2024-06-15 18:07:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
+import os
+from openai import OpenAI
+
 import streamlit as st
 
-st.write('동물 이미지 가져오기 🙂');
+os.environ["OPENAI_API_KEY"] = st.secrets['API_KEY']
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-title = st.text_input("영어로 동물이름 찾기", "")
+st.title('슈퍼 시나리오 봇 🙂');
 
-if st.button('찾아보기') : 
-    url = 'https://edu.spartacodingclub.kr/random/?' + title
-    st.image(url)
+keyword = st.text_input("키워드 입력하세요.")
+
+if st.button('생성하기') : 
+    with st.spinner('생성 중입니다!') : 
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": keyword,
+                },
+                {
+                    "role": "system",
+                    "content": "입력 받은 키워드에 대한 흥미진진한 시나리오를 300자 내외로 만들어줘.",
+                }
+            ],
+            model="gpt-4o",
+        )
+
+    result = chat_completion.choices[0].message.content;
+    st.write(result);
