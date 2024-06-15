9c670bcc8463ac04a344e8b5906abee63c6936fa
jeon1109 <101310070+jeon1109@users.noreply.github.com>
2024-06-15 16:55:28 +0900

    AI 첫 업로드

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write('동물 이미지 가져오기 🙂');
+
+title = st.text_input("영어로 동물이름 찾기", "")
+
+if st.button('찾아보기') : 
+    url = 'https://edu.spartacodingclub.kr/random/?' + title
+    st.image(url)
+
+
+
